9ceb9b2a01621d9eca8bde2c9eca08d9a8ed83c6
wangzhp <478253384@qq.com>
2023-08-17 20:10:05 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -91,7 +91,7 @@ $(function() {
 		  if(topic){
 			  if(isShowPic){
 			 picDiv="<div><img style='width:270px;height:120px;' src='"+hotSearchList[i].pic+"'/></div>"
-			 topicDiv ="<div style='text-indent:2em;font-size: 17px;'>"+topic+"<br/></div>"
+			 topicDiv ="<div style='text-indent:2em;font-size: 17px;'>"+topic+"<br/><br/></div>"
 			  }
 		     
 		  }
@@ -363,7 +363,7 @@ function executeScriptToCurrentTab(code)
          console.log("feed_list_content:"+topic);
        }
 	 }
-	 $hotTopNews.after("<div style='"+style+"'>     "+topic+"<br/></div>");
+	 $hotTopNews.after("<div style='"+style+"'>     "+topic+"<br/><br/></div>");
 	  }});
 	}
 
