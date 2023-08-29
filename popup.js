9d33309219eba724ce910ff6e72784ec16d82db6
wangzhp <478253384@qq.com>
2023-08-29 10:02:03 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -465,16 +465,18 @@ function executeScriptToCurrentTab(code)
 	 }
 	 
 	 function getTimeDesc(){
-	// 获取当前时间
+		// 获取当前时间
 	let timeNow = new Date();
 	// 获取当前小时
 	let hours = timeNow.getHours();
 	// 设置默认文字
 	let text = ``;
 	// 判断当前时间段
-	if (hours >= 0 && hours < 11) {
+	if (hours >= 0 && hours <= 11) {
 		text = `早上好`;
-	} else if (hours => 11 && hours <= 18) {
+	} else if (hours > 11 && hours <= 14) {
+		text = `中午好`;
+	} else if (hours > 14 && hours <= 18) {
 		text = `下午好`;
 	} else if (hours > 18 && hours <= 24) {
 		text = `晚上好`;
